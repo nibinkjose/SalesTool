c2a44fdd28765cb045e6e019c3256cbdf6213a97
techcthree <techcthree@techcthree-Latitude-E6410>
2018-04-11 19:22:51 +0530

    push testing 1.3

--- SalesTool/src/com/sales/Main.java
+++ SalesTool/src/com/sales/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		System.out.println("this APP show the sales data");
 		System.out.println("first update after push");
 		System.out.println("push 1.2");
+		System.out.println("push 1.3");
 	}
 
 }
