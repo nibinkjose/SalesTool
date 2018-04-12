8c52591e3410244c356f4de5572868b235e3dc74
techcthree <techcthree@techcthree-Latitude-E6410>
2018-04-12 16:56:00 +0530

    display data work completed

--- SalesTool/src/com/sales/SalesData.java
+++ SalesTool/src/com/sales/SalesData.java
@@ -4,9 +4,10 @@ public class SalesData {
 	int data[] = {0,4,42};
 	public void display()
 	{
+		System.out.println("data");
 		for(int i=0;i<data.length;i++)
 		{
-			System.out.println("");
+			System.out.println(data[i]);
 		}
 	}
 
